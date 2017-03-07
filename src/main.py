09421aa34651e8f4b9b0495fe65c4c330e536d4e
IndexTyl0rz <tyler32bit@gmail.com>
2017-03-06 20:56:23 -0800

    Updating main.py docstrings.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-   This module represents the first pass of an easy to use image recognition package for highlighting regions of interest and
-   applying filters for further analysis. 
+"""Pull it all together. Ultimately this should be renamed to reflect the name of the package.
+
+This module represents the first pass of an easy to use image recognition package for highlighting regions of interest and
+applying filters for further analysis. 
 """
 
 
@@ -14,39 +15,39 @@ import cv2
 from videoContainer import VideoContainer
 
 
-"""First we setup an argparser for handling commandline opvideo. Yanking this boilerplate from img suff."""
-parser = argparse.ArgumentParser(prog='main', description='Accept a /path/video with --video arg. Some foundation for further computer vision development is provided in the existing code base. Much further work is required but I want the basic structure for easily loading a video file or stream which will then have some regions or interest and filters applied. Next we want to grab the regions of interest and perform further contour analysis on the groupings of pixelzzz.')
+#First we setup an argparser for handling commandline video options.
+parser = argparse.ArgumentParser(prog='main', description='Accept a /path/video with --video arg.\
+                                              Foundation for further computer vision development. \
+                                              Currently we can easily load a video file, apply filters and background \
+                                              separation, identify contours and draw a box around them. Next steps \
+                                              include contour analysis refinement. and Unittests.') 
 parser.add_argument('--video', '-v', type=str , nargs='+', help='Accept a video with a path and do some cv')
 parser.add_argument('--min_area', '-m', type=int, default=500,  nargs='+', help='Set the minimum area for foreground tracking')
 
 args= parser.parse_args() 
 
 
-
 def main(args):
-    """Accep a video path and we will pull strings on OpenCv to draw boxes on moving groups of pixels."""
-    #TODO: All of the things
+    """Accep a video path and we will pull strings on OpenCv to draw boxes on moving groups of pixels.
+    
+    A loop needs to run in order to process each frame of video. I keep the loop at a high level
+    in order to reduce statefullness further down the stack.
+    """
  
     inVid = args.video[0]
-    #minArea = args.min_area 
-    firstFrame = None
-
     staticVideoContainerOne = VideoContainer(inVid)
     
     while(1):
         staticVideoContainerOne.run_video()
 
 
-
 if __name__=='__main__':
     try:
         sys.argv[1]
-    
     except IndexError:
         parser.print_help()
         sys.exit(1)
    
     main(args)
-
 
 
