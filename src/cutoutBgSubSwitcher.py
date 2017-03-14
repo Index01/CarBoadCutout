5607292c1fc8adc69828e38155b1f55508a4ae69
IndexTyl0rz <tyler32bit@gmail.com>
2017-03-14 14:51:06 -0700

    And thats what happens when you test in master. Fixed BgSub short.

--- src/cutoutBgSubSwitcher.py
+++ src/cutoutBgSubSwitcher.py
@@ -2,13 +2,19 @@
 """ Switch between different background subtraction algorithms
 
 OpenCv and other vision tools support a varity of background subtraction algorithms
-which is an important part in isolation regions of interest. 
+which is an important part in isolating regions of interest. This module should 
+allow us to switch between several BgSub algorithms and abstract away dependencies.
+I want to keep this module to functional programming, from the perspective of the
+videoPlayer which will be using these functions, the calling object should just say 
+hey awesome funtion here is some value, give me somthing back. Calling objects must 
+also setattr() for the algorithm it wants, this part may get a better interface 
+definition in the future. 
 """
 
 import cv2
 
 
-bgSubtractAlg = None
+bgSubtractAlg = None    #setattr() this value from calling object.
 sbtktBgInstance = None
 check_is_bgInst = lambda instArg: instArg is sbtktBgInstance
 
@@ -21,24 +27,22 @@ def apply_bg_subtract(frameIn):
         outFrame of type cv.[backgroung subtractor].apply, or None if bogus alorithm specified.         
 
     """
-    bg = lambda x : x
+    outFrame = lambda x : x
     global sbtktBgInstance
-    outFrame = None
+    #TODO: Move this functionality up to the setattr(), caller should immdeiate except.
     try:
-        bg = globals()[bgSubtractAlg]
+        outFrame = globals()[bgSubtractAlg]
     except KeyError, e:
         print "Sorry brah, unsupported algorithm. Exception: %s" % e
 
-    outFrame = bg(frameIn)
-
-    return outFrame
+    return outFrame(frameIn)
 
 
 def mog():
     pass
 
 def mog2(frameIn):
-     """MOG2 algorithm.
+    """MOG2 algorithm.
 
     Args:
         Param1 (frame): Frame for openCV 
@@ -50,21 +54,15 @@ def mog2(frameIn):
     #sbtktBgInstanceMog2 = cv2.createBackgroundSubtractorMOG2()
     if not sbtktBgInstance:
         sbtktBgInstance =  cv2.createBackgroundSubtractorMOG2()
-
     outFrame = sbtktBgInstance.apply(frameIn)
  
     return outFrame
+
 
 def gmg():
     pass
 
 
-
-
-def diff_some_frames():
+def knn():
     pass
-
-def other_awesome_stuff():
-    pass
-
 
